dadc01107fb286866662b4dfaa6cdc8ddd265ab7
alexander.thoms <alexander.thoms@Beris093.beris-consulting.de>
2021-07-09 12:31:06 +0200

    Initial Commit

---
+++ src/main/java/de/clockwise/ClockwiseApplication.java
@@ -0,0 +1,13 @@
+package de.clockwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/clockwise/ClockwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.clockwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
